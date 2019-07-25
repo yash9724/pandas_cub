f21f93a32ed2f4542804431958c8563f8cf0e392
yash9724 <yashgupta9724@gmail.com>
2019-07-25 11:55:27 +0530

    selection and aggregation methods added

--- pandas_cub/__init__.py
+++ pandas_cub/__init__.py
@@ -169,12 +169,186 @@ class DataFrame:
         # select multiple columns -> df[['col1','col2','col3']]
         if isinstance(item, list):
             return DataFrame({col: self._data[col] for col in item})
+        
+        # select specific rows using boolean selection
+        if isinstance(item, DataFrame):
+            if item.shape[1] != 1:
+                raise ValueError('Can only pass a one column DataFrame for selection')
+        
+            bool_arr = next(iter(item._data.values()));
+            if bool_arr.dtype.kind != 'b':
+                raise TypeError('DataFrame must be a boolean')
             
-    def StringMethods(self):
-        pass
-
+            new_data = {}
+            for col, values in self._data.items():
+                new_data[col] = values[bool_arr]
+            return DataFrame(new_data)
     
-    def _add_docs():
-        pass
+        
+        if isinstance(item, tuple):
+            return self._getitem_tuple(item)
+        else:
+            raise TypeError('Select with either a string, a list, or a row and column '
+                            'simultaneous selection')
+        
+    
+    def _getitem_tuple(self, item):
+        # simultaneous selection of rows and cols -> df[rs, cs]
+        if len(item) != 2:
+            raise ValueError('Pass either a single string or a two-item tuple inside the '
+                                'selection operator.')
+        
+        row_selection = item[0];
+        col_selection = item[1]
+
+        if isinstance(row_selection, int):
+            row_selection = [row_selection]
+        elif isinstance(row_selection, DataFrame):
+            if row_selection.shape[1] != 1:
+                raise ValueError('Can only pass a one column DataFrame for selection')
+            row_selection = next(iter(row_selection._data.values()))
+            if row_selection.dtype.kind != 'b':
+                raise TypeError('DataFrame must be a boolean')
+        elif not isinstance(row_selection, (list, slice)):
+            raise TypeError('Row selection must be either an int, slice, list or DataFrame.')
+
+        
+        if isinstance(col_selection, int):
+            col_selection = [self.columns[col_selection]]
+        elif isinstance(col_selection, str):
+            col_selection = [col_selection]
+        elif isinstance(col_selection, list):
+            new_col_selection = []
+            for col in col_selection:
+                if isinstance(col, int):
+                    new_col_selection.append(self.columns[col])
+                else:
+                    new_col_selection.append(col)
+            col_selection = new_col_selection
+        elif isinstance(col_selection, slice):
+            start = col_selection.start
+            stop = col_selection.stop
+            step = col_selection.step
+
+            if isinstance(start, str):
+                start = self.columns.index(col_selection.start)
+            if isinstance(stop, str):
+                stop = self.columns.index(col_selection.stop) + 1
+
+            col_selection = self.columns[start:stop:step]
+        else:
+            raise TypeError('Column selection must be either an int, string, list, or slice')
+
+        new_data = {}
+        for col in col_selection:
+            new_data[col] = self._data[col][row_selection]
+        return DataFrame(new_data)
+            
+
+
+    def _ipython_key_completions_(self):
+        # allows for tab completion when doing df['c
+        return self.columns
+
+    def __setitem__(self, key, value):
+        # adds a new column or a overwrites an old column
+        if not isinstance(key, str):
+            raise NotImplementedError('Only able to set a single column')
+        
+        if isinstance(value, np.ndarray):
+            if value.ndim != 1:
+                raise ValueError('Setting array must be 1D')
+            if len(value) != len(self):
+                raise ValueError('Setting array must be same length as DataFrame')
+        elif isinstance(value, DataFrame):
+            if value.shape[1] != 1:
+                raise ValueError('Setting DataFrame must be one column')
+            if len(value) != len(self):
+                raise ValueError('Setting and Calling DataFrames must be the same length')
+            value = next(iter(value._data.values()))
+        elif isinstance(value,(int, float, bool, str)):
+            value = np.repeat(value,len(self))
+        else:
+            raise TypeError('Setting value must either be a numpy array, '
+                            'DataFrame, integer, string, float, or boolean')
+        
+        if value.dtype.kind == 'U':
+            value = value.astype('O')
+        
+        self._data[key] = value
+
+    def head(self, n=5):
+        return self[:n , :]
+
+    def tail(self, n=5):
+        return self[-n:,:]
+
+    #### Aggregation Methods ####
+
+    def min(self):
+        return self._agg(np.min)
+
+    def max(self):
+        return self._agg(np.max)
+
+    def mean(self):
+        return self._agg(np.mean)
+
+    def median(self):
+        return self._agg(np.median)
+
+    def sum(self):
+        return self._agg(np.sum)
+
+    def var(self):
+        return self._agg(np.var)
+
+    def std(self):
+        return self._agg(np.std)
+
+    def all(self):
+        return self._agg(np.all)
+
+    def any(self):
+        return self._agg(np.any)
+
+    def argmax(self):
+        return self._agg(np.argmax)
+
+    def argmin(self):
+        return self._agg(np.argmin)
+
+    def _agg(self, aggfunc):
+        
+        new_data = {}
+        for col, values in self._data.items():
+            try:
+                val = aggfunc(values)
+            except TypeError:
+                continue
+            new_data[col] = np.array([val])
+        return DataFrame(new_data)
+
+    def isna(self):
+       
+        new_data = {}
+        for col, values in self._data.items():
+            kind = values.dtype.kind
+            if kind == 'O':
+                new_data[col] = values == None 
+            else:
+                new_data[col] = np.isnan(values)
+        return DataFrame(new_data)
+
+    # def count(self):
+    #     new_data = {}
+    #     df = self.isna()
+    #     length = len(self)
+    #     for col, values in self._data.items():
+    #         val = length - values.sum()
+    #         new_data[col] = np.array([val])
+    #     return DataFrame(new_data)
+            
+    
 
     
--- tests/test_dataframe.py
+++ tests/test_dataframe.py
@@ -114,4 +114,261 @@ class TestSelection:
         df_answer = pdc.DataFrame({'a': a, 'c': c})
         assert_df_equals(df_result, df_answer)
 
+    def test_simple_boolean(self):
+        bool_arr = np.array([True, False, False])
+        df_bool = pdc.DataFrame({'col': bool_arr})
+        df_result = df[df_bool]
+        df_answer = pdc.DataFrame({'a': a[bool_arr], 'b': b[bool_arr], 
+                                   'c': c[bool_arr], 'd': d[bool_arr], 
+                                   'e': e[bool_arr]})
+        assert_df_equals(df_result, df_answer)
 
+        with pytest.raises(ValueError):
+            df_bool = pdc.DataFrame({'col': bool_arr, 'col2': bool_arr})
+            df[df_bool]
+
+        with pytest.raises(TypeError):
+            df_bool = pdc.DataFrame({'col': np.array[1, 2, 3]})
+
+    def test_one_column_tuple(self):
+        assert_df_equals(df[:, 'a'], pdc.DataFrame({'a': a}))
+
+    def test_multiple_columns_tuple(self):
+        cols = ['a', 'c']
+        df_result = df[:, cols]
+        df_answer = pdc.DataFrame({'a': a, 'c': c})
+        assert_df_equals(df_result, df_answer)
+
+    def test_int_selcetion(self):
+        assert_df_equals(df[:, 3], pdc.DataFrame({'d': d}))
+
+    def test_simultaneous_tuple(self):
+        with pytest.raises(TypeError):
+            s = set()
+            df[s]
+
+        with pytest.raises(ValueError):
+            df[1, 2, 3]
+
+    def test_single_element(self):
+        df_answer = pdc.DataFrame({'e': np.array([2])})
+        assert_df_equals(df[1, 'e'], df_answer)
+
+    def test_all_row_selections(self):
+        df1 = pdc.DataFrame({'a': np.array([True, False, True]),
+                             'b': np.array([1, 3, 5])})
+        with pytest.raises(ValueError):
+            df[df1, 'e']
+
+        with pytest.raises(TypeError):
+            df[df1['b'], 'c']
+
+        df_result = df[df1['a'], 'c']
+        df_answer = pdc.DataFrame({'c': c[[True, False, True]]})
+        assert_df_equals(df_result, df_answer)
+
+        df_result = df[[1, 2], 0]
+        df_answer = pdc.DataFrame({'a': a[[1, 2]]})
+        assert_df_equals(df_result, df_answer)
+
+        df_result = df[1:, 0]
+        assert_df_equals(df_result, df_answer)
+
+    def test_list_columns(self):
+        df_answer = pdc.DataFrame({'c': c, 'e': e})
+        assert_df_equals(df[:, [2, 4]], df_answer)
+        assert_df_equals(df[:, [2, 'e']], df_answer)
+        assert_df_equals(df[:, ['c', 'e']], df_answer)
+
+        df_result = df[2, ['a', 'e']]
+        df_answer = pdc.DataFrame({'a': a[[2]], 'e': e[[2]]})
+        assert_df_equals(df_result, df_answer)
+
+        df_answer = pdc.DataFrame({'c': c[[1, 2]], 'e': e[[1, 2]]})
+        assert_df_equals(df[[1, 2], ['c', 'e']], df_answer)
+
+        df1 = pdc.DataFrame({'a': np.array([True, False, True]),
+                             'b': np.array([1, 3, 5])})
+        df_answer = pdc.DataFrame({'c': c[[0, 2]], 'e': e[[0, 2]]})
+        assert_df_equals(df[df1['a'], ['c', 'e']], df_answer)
+
+    def test_col_slice(self):
+        df_answer = pdc.DataFrame({'a': a, 'b': b, 'c': c})
+        assert_df_equals(df[:, :3], df_answer)
+
+        df_answer = pdc.DataFrame({'a': a[::2], 'b': b[::2], 'c': c[::2]})
+        assert_df_equals(df[::2, :3], df_answer)
+
+        df_answer = pdc.DataFrame({'a': a[::2], 'b': b[::2], 'c': c[::2], 'd': d[::2], 'e': e[::2]})
+        assert_df_equals(df[::2, :], df_answer)
+
+        with pytest.raises(TypeError):
+            df[:, set()]
+
+    def test_tab_complete(self):
+        assert ['a', 'b', 'c', 'd', 'e'] == df._ipython_key_completions_()
+
+    def test_new_column(self):
+        df_result = pdc.DataFrame({'a': a, 'b': b, 'c': c, 'd': d, 'e': e})
+        f = np.array([1.5, 23, 4.11])
+        df_result['f'] = f
+        df_answer = pdc.DataFrame({'a': a, 'b': b, 'c': c, 'd': d, 'e': e, 'f': f})
+        assert_df_equals(df_result, df_answer)
+
+        df_result = pdc.DataFrame({'a': a, 'b': b, 'c': c, 'd': d, 'e': e})
+        df_result['f'] = True
+        f = np.repeat(True, 3)
+        df_answer = pdc.DataFrame({'a': a, 'b': b, 'c': c, 'd': d, 'e': e, 'f': f})
+        assert_df_equals(df_result, df_answer)
+
+        df_result = pdc.DataFrame({'a': a, 'b': b, 'c': c, 'd': d, 'e': e})
+        f = np.array([1.5, 23, 4.11])
+        df_result['c'] = f
+        df_answer = pdc.DataFrame({'a': a, 'b': b, 'c': f, 'd': d, 'e': e})
+        assert_df_equals(df_result, df_answer)
+
+        with pytest.raises(NotImplementedError):
+            df[['a', 'b']] = 5
+        
+        with pytest.raises(ValueError):
+            df['a'] = np.random.rand(5, 5)
+
+        with pytest.raises(ValueError):
+            df['a'] = np.random.rand(5)
+
+        with pytest.raises(ValueError):
+            df['a'] = df[['a', 'b']]
+
+        with pytest.raises(ValueError):
+            df1 = pdc.DataFrame({'a': np.random.rand(5)})
+            df['a'] = df1
+
+        with pytest.raises(TypeError):
+            df['a'] = set()
+
+    def test_head_tail(self):
+        df_result = df.head(2)
+        df_answer = pdc.DataFrame({'a': a[:2], 'b': b[:2], 'c': c[:2],
+                                   'd': d[:2], 'e': e[:2]})
+        assert_df_equals(df_result, df_answer)
+
+        df_result = df.tail(2)
+        df_answer = pdc.DataFrame({'a': a[-2:], 'b': b[-2:], 'c': c[-2:],
+                                   'd':d[-2:], 'e': e[-2:]})
+        assert_df_equals(df_result, df_answer)
+
+
+a1 = np.array(['a', 'b', 'c'])
+b1 = np.array([11, 5, 8])
+c1 = np.array([3.4, np.nan, 5.1])
+df1 = pdc.DataFrame({'a': a1, 'b': b1, 'c': c1})
+
+a2 = np.array([True, False])
+b2 = np.array([True, True])
+c2 = np.array([False, True])
+df2 = pdc.DataFrame({'a': a2, 'b': b2, 'c': c2})
+
+
+class TestAggregation:
+
+
+    def test_min(self):
+        df_result = df1.min()
+        df_answer = pdc.DataFrame({'a': np.array(['a'], dtype='O'),
+                                   'b': np.array([5]),
+                                   'c': np.array([np.nan])})
+        assert_df_equals(df_result, df_answer)
+
+    def test_max(self):
+        df_result = df1.max()
+        df_answer = pdc.DataFrame({'a': np.array(['c'], dtype='O'),
+                                   'b': np.array([11]),
+                                   'c': np.array([np.nan])})
+        assert_df_equals(df_result, df_answer)
+
+    def test_mean(self):
+        df_result = df1.mean()
+        df_answer = pdc.DataFrame({'b': np.array([8.]),
+                                   'c': np.array([np.nan])})
+        assert_df_equals(df_result, df_answer)
+
+    def test_median(self):
+        df_result = df1.median()
+        df_answer = pdc.DataFrame({'b': np.array([8]),
+                                   'c': np.array([np.nan])})
+        assert_df_equals(df_result, df_answer)
+
+    def test_sum(self):
+        df_result = df1.sum()
+        df_answer = pdc.DataFrame({'a': np.array(['abc'], dtype='O'),
+                                   'b': np.array([24]),
+                                   'c': np.array([np.nan])})
+        assert_df_equals(df_result, df_answer)
+
+    def test_var(self):
+        df_result = df1.var()
+        df_answer = pdc.DataFrame({'b': np.array([b1.var()]),
+                                   'c': np.array([np.nan])})
+        assert_df_equals(df_result, df_answer)
+
+    def test_std(self):
+        df_result = df1.std()
+        df_answer = pdc.DataFrame({'b': np.array([b1.std()]),
+                                   'c': np.array([np.nan])})
+        assert_df_equals(df_result, df_answer)
+
+    def test_all(self):
+        df_result = df2.all()
+        df_answer = pdc.DataFrame({'a': np.array([False]),
+                                   'b': np.array([True]),
+                                   'c': np.array([False])})
+        assert_df_equals(df_result, df_answer)
+
+    def test_any(self):
+        df_result = df2.any()
+        df_answer = pdc.DataFrame({'a': np.array([True]),
+                                   'b': np.array([True]),
+                                   'c': np.array([True])})
+        assert_df_equals(df_result, df_answer)
+
+    def test_argmax(self):
+        df_result = df1.argmax()
+        df_answer = pdc.DataFrame({'a': np.array([2]),
+                                   'b': np.array([0]),
+                                   'c': np.array([1])})
+        assert_df_equals(df_result, df_answer)
+
+    def test_argmin(self):
+        df_result = df1.argmin()
+        df_answer = pdc.DataFrame({'a': np.array([0]),
+                                   'b': np.array([1]),
+                                   'c': np.array([1])})
+        assert_df_equals(df_result, df_answer)
+
+
+a3 = np.array(['a', None, 'c'])
+b3 = np.array([11, 5, 8])
+c3 = np.array([3.4, np.nan, 5.1])
+df3 = pdc.DataFrame({'a': a3, 'b': b3, 'c': c3})
+
+a4 = np.array(['a', 'a', 'c'], dtype='O')
+b4 = np.array([11, 5, 5])
+c4 = np.array([3.4, np.nan, 3.4])
+df4 = pdc.DataFrame({'a': a4, 'b': b4, 'c': c4})
+
+
+class TestOtherMethods:
+
+    def test_isna(self):
+        df_result = df3.isna()
+        df_answer = pdc.DataFrame({'a': np.array([False, True, False]),
+                                   'b': np.array([False, False, False]),
+                                   'c': np.array([False, True, False])})
+        assert_df_equals(df_result, df_answer)
+
+    # def test_count(self):
+    #     df_result = df3.count()
+    #     df_answer = pdc.DataFrame({'a': np.array([2]),
+    #                                'b': np.array([3]),
+    #                                'c': np.array([2])})
+    #     assert_df_equals(df_result, df_answer)
